7c75e9676f318186f1a029830803384bb1018be3
kenhkythuat <thuanphat7.embedded@gmail.com>
2024-09-29 10:03:46 +0700

    clean code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -38,14 +38,6 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-//int _write(int file, char *ptr, int len) {
-//
-//  int i;
-//  for (i = 0; i < len; i++) {
-//    ITM_SendChar(*ptr++);
-//  }
-//  return len;
-//}
 
 /* USER CODE END PD */
 
